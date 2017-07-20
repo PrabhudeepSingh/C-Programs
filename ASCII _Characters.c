dd4dcf0a1ab70979a29463d9a02f723b04d26525
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-07-21 00:13:04 +0530

    Get ASCII value of a char.

---
+++ ASCII _Characters.c
@@ -0,0 +1,13 @@
+//Program to convert chracter to ASCII codes
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char ch;
+	
+	printf("Enter a charater : ");
+	ch = getche(); 	
+	printf("\n\nASCII value : %d",ch);
+
+return 0;
+}
