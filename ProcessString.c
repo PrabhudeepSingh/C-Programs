3aa5f0fc0c5ffa3008055a884a00c2ee96cf2950
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-07-25 10:44:45 +0530

    Process a String using while loop.

---
+++ ProcessString.c
@@ -0,0 +1,14 @@
+//Program to demonstrate Subscripted method to process a string
+#include<stdio.h>
+int main()
+{
+	char s[30]="I am a Sikh";
+	int i=0;
+	while(s[i]!='\0')
+	{
+		printf("%c",s[i]);
+		i++;
+	}
+	
+return 0;
+}
