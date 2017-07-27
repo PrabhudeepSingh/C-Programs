2292d484e951141559c1180c70c5ce38994832d2
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-07-27 11:17:56 +0530

    Demonstrate use of unsigned datatype.

---
+++ UnsignedDatatype.c
@@ -0,0 +1,12 @@
+//To demonstrate the concept of wrapping around
+#include<stdio.h>
+int main()
+{
+	int x=32768;
+	unsigned int y =65536;
+
+	printf("Value stored in x is %d \n",x);
+	printf("Value stored in y is %d \n",y);
+
+	return 0;
+}
