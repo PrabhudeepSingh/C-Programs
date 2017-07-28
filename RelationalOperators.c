83726c5179b73f0900730f7d45f96b6c8b0daba1
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-07-28 12:26:41 +0530

    Demonstrating use of Relational Operators.

---
+++ RelationalOperators.c
@@ -0,0 +1,15 @@
+//Program to demonstrate concept of relational operators
+#include<stdio.h>
+int main()
+{
+	int x=3,y=5;
+	double z=5.37;
+	char c='A';
+	
+	printf("Result 1 = %d",(x+y)>5);
+	printf("\nResult 2 = %d",z>=8.0);
+	printf("\nResult 3 = %d",c<'B');
+	printf("\nResult 4 = %d",x==5);
+	
+	return 0;
+}
