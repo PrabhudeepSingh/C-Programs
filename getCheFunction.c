319372fbaad84f391f84068f0d3c84db4346f1e2
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-07-30 16:30:46 +0530

    Implementing getChe() function.

---
+++ getCheFunction.c
@@ -0,0 +1,14 @@
+//Program to demonstrate the concept of getche() function
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char ch;
+	
+	printf("Enter any character from the keyboard : ");
+	ch=getche();
+	
+	printf("\n\nEntered character is %c",ch);
+
+return 0;
+}
