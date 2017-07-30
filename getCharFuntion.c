8147600408318176efcb0f4e4da723928943746a
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-07-30 16:30:12 +0530

    Implementing getChar() function.

---
+++ getCharFuntion.c
@@ -0,0 +1,14 @@
+//Program to demonstrate the concept of getchar() function
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char ch;
+	
+	printf("Enter any character from the keyboard : ");
+	ch=getchar();
+	
+	printf("\n\nEntered character is %c",ch);
+
+return 0;
+}
