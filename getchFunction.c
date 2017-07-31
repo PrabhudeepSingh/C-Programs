29404e88588cb60577a43c5fec176c8972fcd10e
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-07-31 17:05:15 +0530

    Implementing getCh() function.

---
+++ getchFunction.c
@@ -0,0 +1,14 @@
+//Program to demonstrate the concept of getch() function
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char ch;
+	
+	printf("Enter any character from the keyboard : ");
+	ch=getch();
+	
+	printf("\n\nEntered character is %c",ch);
+
+return 0;
+}
