149e0071505162cd02de297e959c160d599aeac6
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-01 18:36:24 +0530

    Illustrate use of printf Function.

---
+++ printfFunction.c
@@ -0,0 +1,13 @@
+//Program to perform calculations in pritnf() function
+#include<stdio.h>
+int main()
+{
+	float r;
+	
+	printf("Enetr the radius of the circle : ");
+	scanf("%f",&r);
+	
+	printf("\n\nCircumference of circle = %.2f\nArea of circle = %.2f",2*3.14*r,3.14*r*r);
+	
+return 0;
+}
