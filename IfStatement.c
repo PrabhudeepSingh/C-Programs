f00c9ae87d379095da2382532094487b57c6ea35
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-02 15:21:38 +0530

    Demonstrate the use of If Statement.

---
+++ IfStatement.c
@@ -0,0 +1,16 @@
+//Program to demontrate the use of if statement
+#include<stdio.h>
+int main()
+{
+	float n;
+	
+	printf("Enetr any number : ");
+	scanf("%f",&n);
+	
+	if(n<0)
+	   printf("\nEntered number is negative");
+	   
+	printf("\nEnd of program");
+
+return 0;
+}
