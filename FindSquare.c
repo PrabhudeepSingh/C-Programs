5d56981376b9da095682b63cdc368f3da322639e
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-04 09:30:44 +0530

    Find Square of first three natural numbers.

---
+++ FindSquare.c
@@ -0,0 +1,14 @@
+//Program to calculate square of first 3 natural numbers
+#include<stdio.h>
+int main()
+{
+	int a=1;
+	
+	printf("Square of %d is = %d",a,a*a);
+	a=a+1;
+	printf("\nSquare of %d is = %d",a,a*a);
+	a=a+1;
+    printf("\nSquare of %d is = %d",a,a*a);
+
+return 0;
+}
