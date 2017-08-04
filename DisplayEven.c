779385a9b705f0fc8b6325a0a9115534f6a2480d
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-04 09:32:05 +0530

    Display first five Even Numbers.

---
+++ DisplayEven.c
@@ -0,0 +1,15 @@
+//Program to display first five even numbers using while loop.
+#include<stdio.h>
+int main()
+{
+	int n=1,m=0;
+	
+	while(n<=5)
+	{
+		m+=2;
+		printf("%d\n",m);
+		n+=1;
+	}
+	
+return 0;
+}
