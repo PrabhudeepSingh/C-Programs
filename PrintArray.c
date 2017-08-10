98b91de8e97d9f4d707921e0b52afdea2cb63697
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-10 08:04:54 +0530

    To print Array elements.

---
+++ PrintArray.c
@@ -0,0 +1,11 @@
+//Program to define and initialize an element
+#include<stdio.h>
+int main()
+{
+	int a[5]={4,3,9,6,2},i;
+	
+	for(i=0;i<5;i++)
+	   printf("a[%d] = %d\n",i,a[i]);
+	   
+return 0;
+}
