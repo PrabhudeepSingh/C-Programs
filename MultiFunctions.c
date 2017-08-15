119dd4b9275f84ef2ad3c91d6ce6185d4aa024c5
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-15 09:40:20 +0530

    To illustrate the concept of multi-function Program.

---
+++ MultiFunctions.c
@@ -0,0 +1,21 @@
+//To illustrate the concept of multi-function program.
+#include<stdio.h>
+
+int display()
+{
+	for(int i=0;i<=50;i++)
+    	printf("-");
+    printf("\n");
+
+return 0;    
+}
+
+int main()
+{
+	
+	display();
+	printf("		WELCOME		WELCOME\n");
+	display();
+	
+return 0;
+}
