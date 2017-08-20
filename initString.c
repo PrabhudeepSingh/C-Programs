07430cbcb170eb7f03f8a575326fa4453149d9dc
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-20 12:19:24 +0530

    Program to initialize a String.

---
+++ initString.c
@@ -0,0 +1,13 @@
+//Program to initialize a string
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char ch[10]="COMPUTER";
+	char ch1[10]={'B','O','O','K','\0'};
+	
+	puts(ch);
+	puts(ch1);
+	
+return 0;
+}
