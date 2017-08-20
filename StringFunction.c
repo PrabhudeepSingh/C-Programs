ea2e65c9e3fcf8d25c3fb79258898474870927c7
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-20 12:18:43 +0530

    Program to declare, input and display a string.

---
+++ StringFunction.c
@@ -0,0 +1,13 @@
+//Program to declare, input and display a string
+#include<stdio.h>
+int main()
+{
+	char ch[50];
+	
+	printf("Enter the String : ");
+	scanf("%s",&ch);
+	
+	printf("\n\nString is : %s",ch);
+	
+return 0;
+}
