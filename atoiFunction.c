aa449ae282be02e13dde33475a785899225db673
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-08-21 09:20:46 +0530

    Program to demonstrate atoi function.

---
+++ atoiFunction.c
@@ -0,0 +1,13 @@
+//Program to demonstrate atoi function
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	char a[5]="10";
+	int b;
+	b=atoi(a);
+	b+=2;
+	printf("Answere is : %d",b);
+	
+return 0;
+}
